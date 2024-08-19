6a9eab57b9daa01064618a51d8e280943d005670
dipak-01 <imt_2022037@iiitm.ac.in>
2024-08-19 23:00:55 +0530

    refactor: for vercel

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-import express from "express";
+import bcryptjs from "bcryptjs";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
-import userRoute from "./route/user.route.js";
 import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
 
 dotenv.config(); // Load environment variables
 
@@ -13,20 +13,91 @@ app.use(express.json());
 const port = process.env.PORT || 4000;
 const URI = process.env.MongoDBURI;
 
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
 const connectToMongoDB = async () => {
-    try {
-        await mongoose.connect(URI);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        process.exit(1); // Exit the process if unable to connect
-    }
+  try {
+    await mongoose.connect(URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process if unable to connect
+  }
 };
-
+const userSchema = mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+const User = mongoose.model("User", userSchema);
 connectToMongoDB();
 
-app.use("/user", userRoute);
+const signup = async (req, res) => {
+  try {
+    const { fullname, email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const hashPassword = await bcryptjs.hash(password, 10);
+    const createdUser = new User({
+      fullname: fullname,
+      email: email,
+      password: hashPassword,
+    });
+    await createdUser.save();
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        _id: createdUser._id,
+        fullname: createdUser.fullname,
+        email: createdUser.email,
+      },
+    });
+  } catch (error) {
+    console.log("Error: " + error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!user || !isMatch) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    } else {
+      res.status(200).json({
+        message: "Login successful",
+        user: {
+          _id: user._id,
+          fullname: user.fullname,
+          email: user.email,
+        },
+      });
+    }
+  } catch (error) {
+    console.log("Error: " + error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+const router = express.Router();
+router.post("/signup", signup);
+router.post("/login", login);
+const userRoute = router;
+export default userRoute;
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+// Use the router in your app
+
+app.use("/user", userRoute);
